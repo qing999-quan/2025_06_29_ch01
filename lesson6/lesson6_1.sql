381b5abef59cfbc996939ce9d019b63995928c04
qing999-quan <chin299020@gmail.com>
2025-07-13 07:41:44 +0000

    第5次提交

--- lesson6/lesson6_1.sql
+++ lesson6/lesson6_1.sql
@@ -3,3 +3,7 @@ ALTER TABLE "台鐵車站資訊" ADD PRIMARY KEY ("stationCode");
 
 ALTER TABLE "每日各站進出站人數" ADD FOREIGN KEY ("車站代碼")
 REFERENCES "台鐵車站資訊"("stationCode");
+
+SELECT count(*) AS "總筆數"
+FROM "每日各站進出站人數";
+
