c39e843f19b5c59a711887e53fd9e5d72d76f5f3
qing999-quan <chin299020@gmail.com>
2025-08-03 08:14:51 +0000

    建立db

--- lesson9/db.py
+++ lesson9/db.py
@@ -32,11 +32,3 @@ def get_all_stations():
     cursor.close()
     conn.close()
     return result
-
-def main():
-    stations = get_all_stations()
-    print("所有台鐵車站資訊的站點名稱：", stations)
-
-
-if __name__ == "__main__":
-    main()
