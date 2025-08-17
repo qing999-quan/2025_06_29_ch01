1ad1656226c2a8ecc3036b1ed695d8de0b8067c8
qing999-quan <chin299020@gmail.com>
2025-08-17 07:45:25 +0000

    建立11_1

--- lesson11/datasource.py
+++ lesson11/datasource.py
@@ -1,6 +1,7 @@
 import psycopg2
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+
 load_dotenv()
 
 def get_stations_names():
@@ -29,6 +30,43 @@ def get_stations_names():
         result_list = [station[0] for station in result]
 
         return result_list
+
+    except psycopg2.Error as e:
+        print(f"資料庫連線或查詢失敗：{e}")
+        return None
+    except Exception as e:
+        print(f"發生未預期的錯誤：{e}")
+        return None
+    finally:
+        # 確保資源正確釋放
+        if 'cursor' in locals():
+            cursor.close()
+        if 'conn' in locals():
+            conn.close()
+
+def get_min_and_max_date():
+    """
+    取得資料表中的最小和最大日期
+    :return: (最小日期, 最大日期)，連線失敗時回傳 None
+    """
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("HOST"),
+            database=os.getenv("DATABASE"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD"),
+            port="5432"
+        )
+
+        cursor = conn.cursor()
+        query = """
+        SELECT MIN("日期") AS min_date, MAX("日期") AS max_date
+        FROM public."每日各站進出站人數";
+        """
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        return result
 
     except psycopg2.Error as e:
         print(f"資料庫連線或查詢失敗：{e}")
---
+++ lesson11/lesson11.sql
@@ -0,0 +1,7 @@
+SELECT t."stationName" AS "車站名稱",
+	p."日期",
+	p."進站人數",
+	p."出站人數"
+FROM "每日各站進出站人數" p
+LEFT JOIN "台鐵車站資訊" t ON p."車站代碼" = t."stationCode"
+WHERE t."stationName" = '基隆' AND p."日期" = '2023-01-01';
